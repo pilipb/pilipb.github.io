3c222dfd27139f4dd0bbac73ea880adc04bca831
pilipb <xq19351@bristol.ac.uk>
2024-01-10 11:56:27 +0000

    about page image and cube removed

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,12 +5,14 @@ import {
   faGitAlt,
   faHtml5,
   faJsSquare,
+  faPython,
   faReact,
 } from '@fortawesome/free-brands-svg-icons'
 import Loader from 'react-loaders'
 import AnimatedLetters from '../AnimatedLetters'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './index.scss'
+import Profile from '../../assets/images/profile_pic.JPG'
 
 const About = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
@@ -24,6 +26,18 @@ const About = () => {
   return (
     <>
       <div className="container about-page">
+        
+        <div className="profile-pic-cont">
+          <img className="profile-pic" 
+          src={Profile} 
+          alt="Profile Pic"
+          // make entire image fit
+          height="100%"
+          
+          />
+
+        </div>
+
         <div className="text-zone">
           <h1>
             <AnimatedLetters
@@ -33,23 +47,25 @@ const About = () => {
             />
           </h1>
           <p>
-            I'm a final year Engineering Design MEng student at the University of
-            Bristol, UK. I'm currently looking for a graduate or junior role.
+            I'm a final year Engineering Design MEng student at the University
+            of Bristol, UK. I'm currently looking for a graduate or junior role.
           </p>
           <p align="LEFT">
-            My University and work experience has given me a strong foundation in software development, namely
-            robotics, optimisation and machine learning / AI. I have a strong interest in mechanical/electronic design and 
-            creative coding.
+            My University and work experience has given me a strong foundation
+            in software development, namely robotics, optimisation and machine
+            learning / AI. I have a strong interest in mechanical/electronic
+            design and creative coding.
           </p>
           <p>
-            I'm currently working on a few projects including a pen plotter, codes for plotting and this website.
+            I'm currently working on a few projects including a pen plotter,
+            codes for plotting and this website.
           </p>
         </div>
 
-        <div className="stage-cube-cont">
+        {/* <div className="stage-cube-cont">
           <div className="cubespinner">
             <div className="face1">
-              <FontAwesomeIcon icon={faAngular} color="#DD0031" />
+              <FontAwesomeIcon icon={faPython} color="#DD0031" />
             </div>
             <div className="face2">
               <FontAwesomeIcon icon={faHtml5} color="#F06529" />
@@ -67,7 +83,7 @@ const About = () => {
               <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
             </div>
           </div>
-        </div>
+        </div> */}
       </div>
       <Loader type="pacman" />
     </>
