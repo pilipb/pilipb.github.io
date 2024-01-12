573bdb7734aa1283c1621711bf421c88ea5ae45f
pilipb <xq19351@bristol.ac.uk>
2024-01-12 17:54:32 +0000

    moved projects to file

---
+++ src/components/Portfolio/data/projects.js
@@ -0,0 +1,45 @@
+import img1 from '../data/plottedcad.png'
+// import img2 from '../data/plotter.png'
+
+export const projects = [
+  {
+    title: 'Pen Plotter',
+    subtitle: 'Design and software for a CNC pen plotter',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?',
+    image: img1,
+    link: 'https://github.com/pilipb/plotter_code',
+  },
+  {
+    title: 'Optimisation',
+    subtitle: 'Matlab',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?',
+    image: './project-2.gif',
+    link: 'https://github.com/pilipb/Optimisation',
+  },
+  {
+    title: 'Transport Modelling',
+    subtitle: 'Matlab',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?',
+    image: './project-3.gif',
+    link: 'https://github.com/pilipb/transport_modelling',
+  },
+  {
+    title: 'Signatures',
+    subtitle: 'Computer vision with Python',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?',
+    image: './project-4.gif',
+    link: 'https://github.com/pilipb/Signatures',
+  },
+  //   {
+  //     title: 'Novel Reinforcement Learning Approach',
+  //     subtitle: 'React Hooks',
+  //     description:
+  //       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?',
+  //     image: './project-4.gif',
+  //     link: 'https://pythonbootcamp.com',
+  //   },
+]
