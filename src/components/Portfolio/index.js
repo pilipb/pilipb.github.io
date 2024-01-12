2950deb6125fc7422cc9b61c7457a60e690b6277
pilipb <xq19351@bristol.ac.uk>
2024-01-12 17:53:48 +0000

    moved projects to file

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -4,6 +4,7 @@ import AnimatedLetters from "../AnimatedLetters";
 import "./index.scss";
 import { getDocs, collection } from 'firebase/firestore';
 import { db } from '../../firebase';
+import { projects } from './data/projects';
 
 const Portfolio = () => { 
     const [letterClass, setLetterClass] = useState('text-animate');
@@ -28,40 +29,7 @@ const Portfolio = () => {
         setPortfolio(querySnapshot.docs.map((doc) => doc.data()));
     }
 
-    const projects = [
-      {
-        title: 'React Reserve',
-        subtitle: 'MERN Stack',
-        description:
-          'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?',
-        image: './project-1.gif',
-        link: 'https://reactbootcamp.com',
-      },
-      {
-        title: 'React Tracks',
-        subtitle: 'React and Python',
-        description:
-          'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?',
-        image: './project-2.gif',
-        link: 'https://reedbarger.com',
-      },
-      {
-        title: 'DevChat',
-        subtitle: 'React and Firebase',
-        description:
-          'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?',
-        image: './project-3.gif',
-        link: 'https://jsbootcamp.com',
-      },
-      {
-        title: 'Epic Todo App',
-        subtitle: 'React Hooks',
-        description:
-          'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?',
-        image: './project-4.gif',
-        link: 'https://pythonbootcamp.com',
-      },
-    ]
+    
 
     const renderPortfolio = (portfolio) => {
         return (
