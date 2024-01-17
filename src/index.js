13da2a1954cdb6016b478118fa23e6f0b7c352d7
pilipb <xq19351@bristol.ac.uk>
2024-01-17 12:46:54 +0000

    updated to use hashrouter

--- src/index.js
+++ src/index.js
@@ -2,14 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import reportWebVitals from './reportWebVitals'
 import { BrowserRouter } from 'react-router-dom'
+import { HashRouter as Router } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Router>
       <App />
-    </BrowserRouter>
+    </Router>
     ,
   </React.StrictMode>,
   document.getElementById('root')
