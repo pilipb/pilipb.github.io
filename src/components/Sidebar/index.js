85e644284eca42acb2a4fc54f3f8c63fd2b52928
pilipb <xq19351@bristol.ac.uk>
2024-05-15 00:20:54 +0100

    remove useless imports

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,7 +1,5 @@
 import './index.scss'
 import { useState } from 'react'
-import LogoS from '../../assets/images/logo-s.png'
-import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
   faLinkedin,
@@ -93,7 +91,6 @@ const Sidebar = () => {
       </ul>
       <FontAwesomeIcon
         onClick={() => setShowNav(true)}
-        onTouchStart={() => setShowNav(true)}
         icon={faBars}
         color="#ff9900"
         size="3x"
